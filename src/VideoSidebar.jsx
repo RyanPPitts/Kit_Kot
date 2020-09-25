3d07ef56eac1ae184361ab0d6dc57015441f336e
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-09-24 18:28:58 -0700

    Adjusting css

--- src/VideoSidebar.jsx
+++ src/VideoSidebar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './VideoSidebar.css';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
 
 function VideoSidebar() {
   return (
     <div className="videoSidebar">
-      <h2> This is the sidebar </h2>{' '}
+      <div className="videoSidebar__button">
+        <FavoriteIcon />
+        <p>500 likes</p>
+      </div>
     </div>
   );
 }
