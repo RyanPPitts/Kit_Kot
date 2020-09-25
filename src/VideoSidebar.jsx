461d53c1f97ab2fe83d891b4af80d0ac9d7130a3
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-09-24 18:53:29 -0700

    OnClick Action : Favorite Icon
    Adding state to the favorite icon

--- src/VideoSidebar.jsx
+++ src/VideoSidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VideoSidebar.css';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
@@ -6,11 +6,24 @@ import MessageIcon from '@material-ui/icons/Message';
 import ShareIcon from '@material-ui/icons/Share';
 
 function VideoSidebar() {
+  const [liked, setLiked] = useState(false);
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        <FavoriteIcon />
-        <p>500 likes</p>
+        {liked ? (
+          <FavoriteIcon onClick={(e) => setLiked(false)} />
+        ) : (
+          <FavoriteBorderIcon onClick={(e) => setLiked(true)} />
+        )}
+        <p>500</p>
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon />
+        <p>200</p>
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon />
+        <p>10</p>
       </div>
     </div>
   );
